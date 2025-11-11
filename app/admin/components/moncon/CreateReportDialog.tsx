1bb769b2461e02066bd142033d8e8f2b03e48fc8
Ivan Diaz <ivan.diaz.leuccalla@gmail.com>
2025-11-11 17:43:13 -0500

    feat: enhance CreateReportDialog with areas and equipments state management

--- app/admin/components/moncon/CreateReportDialog.tsx
+++ app/admin/components/moncon/CreateReportDialog.tsx
@@ -20,9 +20,11 @@ type CreateReportDialogProps = {
 }
 
 const CreateReportDialog = ({ openNewRegister, setOpenNewRegister, getAllReport }: CreateReportDialogProps) => {
- const [entities, setEntities] = useState<Entity[]>([])
- const [date, setDate] = useState<Date | undefined>(new Date())
- const [data, setData] = useState<ReportType>({
+  const [entities, setEntities] = useState<Entity[]>([])
+  const [areas, setAreas] = useState<Entity[]>([])
+  const [equipments, setEquipments] = useState<Entity[]>([])
+  const [date, setDate] = useState<Date | undefined>(new Date())
+  const [data, setData] = useState<ReportType>({
     id: 0,
     entity: 0,
     name: "",
@@ -53,7 +55,6 @@ const CreateReportDialog = ({ openNewRegister, setOpenNewRegister, getAllReport 
   useEffect(() => {
     getEntities()
       .then((response) => {
-        console.log("Fetched entities:", response)
         setEntities(response.data)
       })
       .catch((error) => {
@@ -61,197 +62,185 @@ const CreateReportDialog = ({ openNewRegister, setOpenNewRegister, getAllReport 
       })
   }, [])
 
+  console.log("Entities in dialog:", entities);
+
   return (
-     <Dialog open={openNewRegister} onOpenChange={setOpenNewRegister}>
-        <DialogContent className='min-w-5xl bg-slate-200'>
-          <DialogHeader>
-            <DialogTitle>NUEVO REGISTRO </DialogTitle>
-            <DialogDescription>Nuevo registro no programado que no esta registrado en ruta semanal</DialogDescription>
-          </DialogHeader>
-          <div className='w-full p-2 rounded-md gap-4 flex flex-col mt-4'>
-            <div className='w-full flex flex-row gap-2'>
-              <div className='flex flex-col gap-2 w_-1/3'>
-                <Label className='font-semibold'>Planta:</Label>
-                <Input
-                  className='bg-white' value={String(entities.find(entity => entity.parent === null)?.name)} disabled></Input>
-              </div>
-              <div className='flex flex-col gap-2 w-1/3'>
-                <Label className='font-semibold'>Área:</Label>
-                <Select>
-                  <SelectTrigger className='w-full bg-white'>
-                    <SelectValue placeholder="Seleccionar..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {
-                      entities.filter((entity) => entity.parent === 1).map((entity) => (
-                        <SelectItem key={entity.id} value={String(entity.id)}>{entity.name}</SelectItem>
-                      ))
-                    }
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className='flex flex-col gap-2 w-1/4'>
-                <Label className='font-semibold'>TAG:</Label>
-                <Select
-
-                  value={data ? String(data.entity) : ""}
-                  onValueChange={(value) => {
-                    setData({
-                      ...data!,
-                      entity: parseInt(value)
-                    })
-                  }}
-                >
-                  <SelectTrigger className='bg-white w-full'>
-                    <SelectValue placeholder="Seleccionar..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="1">Componente1</SelectItem>
-                    <SelectItem value="2"></SelectItem>
-                    <SelectItem value="3"></SelectItem>
-                    <SelectItem value="4"></SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className='flex flex-col gap-2 w-2/4'>
-                <Label className='font-semibold'>Nombre de equipo:</Label>
-                <Input className='bg-white' />
-              </div>
+    <Dialog open={openNewRegister} onOpenChange={setOpenNewRegister}>
+      <DialogContent className='min-w-5xl bg-slate-200'>
+        <DialogHeader>
+          <DialogTitle>NUEVO REGISTRO </DialogTitle>
+          <DialogDescription>Nuevo registro no programado que no esta registrado en ruta semanal</DialogDescription>
+        </DialogHeader>
+        <div className='w-full p-2 rounded-md gap-4 flex flex-col mt-4'>
+          <div className='w-full flex flex-row gap-2'>
+            <div className='flex flex-col gap-2 w_-1/3'>
+              <Label className='font-semibold'>Planta:</Label>
+              <Input
+                className='bg-white' value={String(entities.find(entity => entity.parent === null)?.name)} disabled></Input>
             </div>
-            <div className='w-full flex flex-row gap-2'>
-              <div className='flex flex-col gap-2 w-1/3'>
-                <Label className='font-semibold'>Fecha de programación:</Label>
-                <h1 className='p-2 text-sm bg-white rounded-md'>{format(new Date(), "dd/MM/yyyy")}</h1>
-                {/* <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      data-empty={!date}
-                      className="data-[empty=true]:text-muted-foreground w-[280px] justify-start text-left font-normal"
-                    >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {date ? format(date, "dd/MM/yyyy", { locale: es }) : <span>Selecciona una fecha</span>}
-                    </Button>
-                  </PopoverTrigger>
-
-                  <PopoverContent className="w-auto p-0">
-                    <Calendar
-                      mode="single"
-                      selected={date}
-                      onSelect={setDate}
-                      locale={es}
-                      defaultMonth={date} // ✅ Muestra el mes actual
-                    />
-                  </PopoverContent>
-                </Popover> */}
-              </div>
-              <div className='flex flex-col gap-2 w-1/3'>
-                <Label className='font-semibold'>Programación:</Label>
-                <Select
-                  value={data ? String(data.program) : ""}
-                  onValueChange={(value) => {
-                    setData({
-                      ...data!,
-                      program: parseInt(value)
-                    })
-                  }}
-                >
-                  <SelectTrigger className='w-full bg-white'>
-                    <SelectValue placeholder="Seleccionar..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="1">Programado</SelectItem>
-                    <SelectItem value="2">No Programado</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className='flex flex-col gap-2 w-1/3'>
-                <Label className='font-semibold'>Condición:</Label>
-                <Select
-                  value={data ? String(data.condition) : ""}
-                  onValueChange={(value) => {
-                    setData({
-                      ...data!,
-                      condition: parseInt(value)
-                    })
-                  }}
-                >
-                  <SelectTrigger className='w-full bg-white'>
-                    <SelectValue placeholder="Seleccionar..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="1">Normal</SelectItem>
-                    <SelectItem value="2">Tolerable</SelectItem>
-                    <SelectItem value="3">Precacución</SelectItem>
-                    <SelectItem value="4">Crítico</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
+            <div className='flex flex-col gap-2 w-1/3'>
+              <Label className='font-semibold'>Área:</Label>
+              <Select
+                value={data ? String(data.entity) : ""}
+                onValueChange={(value) => {
+                  setData({
+                    ...data!,
+                    entity: parseInt(value)
+                  })
+                }}
+              >
+                <SelectTrigger className='w-full bg-white'>
+                  <SelectValue placeholder="Seleccionar..." />
+                </SelectTrigger>
+                <SelectContent>
+                  {
+                    areas.map((area) => (
+                      <SelectItem key={area.id} value={String(area.id)}>{area.name}</SelectItem>
+                    ))
+                  }
+                </SelectContent>
+              </Select>
             </div>
-            <div className='w-full flex flex-row gap-2'>
-              <div className='flex flex-col gap-2 w-1/4'>
-                <Label className='font-semibold'>Tipo de tarea:</Label>
-                <Select
-                  value={data ? String(data?.task_type) : ""}
-                  onValueChange={(value) => {
-                    setData({
-                      ...data!,
-                      task_type: parseInt(value)
-                    })
-                  }}
-                >
-                  <SelectTrigger className='w-full bg-white'>
-                    <SelectValue placeholder="Seleccionar..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="1">PDM</SelectItem>
-                    <SelectItem value="2">NDT</SelectItem>
-                    <SelectItem value="3">Alineamiento</SelectItem>
-                    <SelectItem value="4">Visual</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className='flex flex-col gap-2 w-1/4'>
-                <Label className='font-semibold'>Status de ejecución:</Label>
-                <Select
-                  value={data ? String(data.execution_status) : ""}
-                  onValueChange={(value) => {
-                    setData({
-                      ...data!,
-                      execution_status: parseInt(value)
-                    })
-                  }}
-                >
-                  <SelectTrigger className='w-full bg-white'>
-                    <SelectValue placeholder="Seleccionar..." />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="1">Ejecutado</SelectItem>
-                    <SelectItem value="2">No ejecutado</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className='flex flex-col gap-2 w-2/4'>
-                <Label className='font-semibold'>Observación:</Label>
-                <Input
-                  className='bg-white'
-                  value={data ? data.observations : ""}
-                  onChange={(e) => {
-                    setData({
-                      ...data!,
-                      observations: e.target.value
-                    })
-                  }}
-                />
-              </div>
+            <div className='flex flex-col gap-2 w-1/4'>
+              <Label className='font-semibold'>TAG:</Label>
+              <Select
+
+                value={data ? String(data.entity) : ""}
+                onValueChange={(value) => {
+                  setData({
+                    ...data!,
+                    entity: parseInt(value)
+                  })
+                }}
+              >
+                <SelectTrigger className='bg-white w-full'>
+                  <SelectValue placeholder="Seleccionar..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">Componente1</SelectItem>
+                  <SelectItem value="2"></SelectItem>
+                  <SelectItem value="3"></SelectItem>
+                  <SelectItem value="4"></SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='flex flex-col gap-2 w-2/4'>
+              <Label className='font-semibold'>Nombre de equipo:</Label>
+              <Input className='bg-white' />
             </div>
           </div>
-          <DialogFooter>
-            <Button onClick={() => createSingleRegister()}>Guardar y salir</Button>
-            {/* <Button>Guardar y agregar nuevo</Button> */}
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+          <div className='w-full flex flex-row gap-2'>
+            <div className='flex flex-col gap-2 w-1/3'>
+              <Label className='font-semibold'>Fecha de programación:</Label>
+              <h1 className='p-2 text-sm bg-white rounded-md'>{format(new Date(), "dd/MM/yyyy")}</h1>
+            </div>
+            <div className='flex flex-col gap-2 w-1/3'>
+              <Label className='font-semibold'>Programación:</Label>
+              <Select
+                value={data ? String(data.program) : ""}
+                onValueChange={(value) => {
+                  setData({
+                    ...data!,
+                    program: parseInt(value)
+                  })
+                }}
+              >
+                <SelectTrigger className='w-full bg-white'>
+                  <SelectValue placeholder="Seleccionar..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">Programado</SelectItem>
+                  <SelectItem value="2">No Programado</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='flex flex-col gap-2 w-1/3'>
+              <Label className='font-semibold'>Condición:</Label>
+              <Select
+                value={data ? String(data.condition) : ""}
+                onValueChange={(value) => {
+                  setData({
+                    ...data!,
+                    condition: parseInt(value)
+                  })
+                }}
+              >
+                <SelectTrigger className='w-full bg-white'>
+                  <SelectValue placeholder="Seleccionar..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">Normal</SelectItem>
+                  <SelectItem value="2">Tolerable</SelectItem>
+                  <SelectItem value="3">Precacución</SelectItem>
+                  <SelectItem value="4">Crítico</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <div className='w-full flex flex-row gap-2'>
+            <div className='flex flex-col gap-2 w-1/4'>
+              <Label className='font-semibold'>Tipo de tarea:</Label>
+              <Select
+                value={data ? String(data?.task_type) : ""}
+                onValueChange={(value) => {
+                  setData({
+                    ...data!,
+                    task_type: parseInt(value)
+                  })
+                }}
+              >
+                <SelectTrigger className='w-full bg-white'>
+                  <SelectValue placeholder="Seleccionar..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">PDM</SelectItem>
+                  <SelectItem value="2">NDT</SelectItem>
+                  <SelectItem value="3">Alineamiento</SelectItem>
+                  <SelectItem value="4">Visual</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='flex flex-col gap-2 w-1/4'>
+              <Label className='font-semibold'>Status de ejecución:</Label>
+              <Select
+                value={data ? String(data.execution_status) : ""}
+                onValueChange={(value) => {
+                  setData({
+                    ...data!,
+                    execution_status: parseInt(value)
+                  })
+                }}
+              >
+                <SelectTrigger className='w-full bg-white'>
+                  <SelectValue placeholder="Seleccionar..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">Ejecutado</SelectItem>
+                  <SelectItem value="2">No ejecutado</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='flex flex-col gap-2 w-2/4'>
+              <Label className='font-semibold'>Observación:</Label>
+              <Input
+                className='bg-white'
+                value={data ? data.observations : ""}
+                onChange={(e) => {
+                  setData({
+                    ...data!,
+                    observations: e.target.value
+                  })
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button onClick={() => createSingleRegister()}>Guardar y salir</Button>
+          {/* <Button>Guardar y agregar nuevo</Button> */}
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 
